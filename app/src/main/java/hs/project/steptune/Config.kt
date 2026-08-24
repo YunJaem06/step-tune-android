71725f58154e2b9872f675e77ea69e58dbf2f316
Viva <yunjae0616@gmail.com>
2026-08-24 15:00:51 +0900

    [chore] API Config 추가

---
+++ app/src/main/java/hs/project/steptune/Config.kt
@@ -0,0 +1,13 @@
+package hs.project.steptune
+
+object Config {
+    val BASE_URL: String = BuildConfig.BASE_URL
+    val GOOGLE_WEB_CLIENT_ID: String = BuildConfig.GOOGLE_WEB_CLIENT_ID
+
+    object API {
+        const val AUTH_SOCIAL = "api/v1/auth/social"
+        const val AUTH_REFRESH = "api/v1/auth/refresh"
+        const val AUTH_LOGOUT = "api/v1/auth/logout"
+        const val MY_INFO = "api/v1/me"
+    }
+}
